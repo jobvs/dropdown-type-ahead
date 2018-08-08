993bc67d2da6f1e2a7b6e2f25bf65c0c91497222
Joseph D <j_seremba@live.com>
2018-08-08 13:40:59 +0300

    Setup functional tests

---
+++ tests/e2e/DropdownTypeaheadReference.spec.ts
@@ -0,0 +1,14 @@
+describe("Dropdown typea head reference", () => {
+
+    it("should change dropdown value", () => {
+        //
+    });
+
+    it("with value can clear value", () => {
+        //
+    });
+
+    it("should populate multiple values in a dropdown list", () => {
+        //
+    });
+});
---
+++ tests/e2e/pages/sample.page.ts
@@ -0,0 +1,11 @@
+// import { Client, Element, RawResult } from "webdriverio";
+
+class SamplePage {
+
+    // public open(): void {
+    //     browser.url("/p/xpath");
+    // }
+}
+
+const samplePage = new SamplePage();
+export default samplePage;
