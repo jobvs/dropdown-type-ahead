0f5a98df21b771a663b36a822270874c9c5c7d86
Stanley-Okwii <stanleeparker12@gmail.com>
2018-08-14 16:12:18 +0300

    Add dropdown indicator component

---
+++ src/components/DropdownIndicator.ts
@@ -0,0 +1,27 @@
+import { SFC, createElement } from "react";
+import { components } from "react-select";
+import * as classNames from "classnames";
+
+export interface DropProps {
+    children?: Node;
+}
+
+export const Dropdown: SFC<DropProps> = (props) => {
+    return createElement(components.DropdownIndicator as any , {
+        ...props,
+        className: "widget-dropdown-type-head-arrow-container"
+    },
+        createElement("svg", {
+            className: classNames("widget-dropdown-type-head-arrow"),
+            focusable: false,
+            height: 20,
+            viewBox: "0 0 20 20",
+            width: 20
+        },
+            createElement("path", {
+                // tslint:disable-next-line:max-line-length
+                d: "M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"
+            })));
+};
+
+Dropdown.displayName = "Dropdown";
