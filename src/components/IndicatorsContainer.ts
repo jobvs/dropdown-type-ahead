d4b12f5a5d6e034d1a146b624e1773b46b974f31
Stanley-Okwii <stanleeparker12@gmail.com>
2018-08-14 16:13:26 +0300

    Add indicator container component

---
+++ src/components/IndicatorsContainer.ts
@@ -0,0 +1,16 @@
+import { SFC, createElement } from "react";
+import { components } from "react-select";
+// import * as classNames from "classnames";
+
+export interface IndicatorsContainerProps {
+    children?: Node;
+}
+
+export const IndicatorsContainer: SFC<IndicatorsContainerProps> = (props) => {
+    return createElement(components.IndicatorsContainer as any , {
+        ...props, className: "widget-dropdown-type-head-indicator-container"
+     }
+    );
+};
+
+IndicatorsContainer.displayName = "IndicatorsContainer";
